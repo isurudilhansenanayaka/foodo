1ed610eee7aa37b088ebf159e62aa740d9f8f9ba
isurudilhansenanayaka <iddmdilhan@gmail.com>
2019-03-18 00:06:19 +0530

    ietm List create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
+import { ItemComponent } from './item/item.component';
+import { TempComponent } from './temp/temp.component';
+import { ItemListComponent } from './item/item-list/item-list.component';
+import { ItemListItemComponent } from './item/item-list-item/item-list-item.component';
+
+const routes: Routes=[
+  {path: '', component: ItemComponent},
+  {path: 'temp', component: TempComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ItemComponent,
+    TempComponent,
+    ItemListComponent,
+    ItemListItemComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule
   ],
   providers: [],
---
+++ src/app/item/item-list-item/item-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-list-item',
+  templateUrl: './item-list-item.component.html',
+  styleUrls: ['./item-list-item.component.css']
+})
+export class ItemListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
