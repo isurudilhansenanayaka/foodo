4c9bb469ca9f35d4c41d7880b62f2f08f1a4186c
isurudilhansenanayaka <iddmdilhan@gmail.com>
2019-03-19 22:05:02 +0530

    update ootstrap

--- src/app/item/item-list-item/item-list-item.component.ts
+++ src/app/item/item-list-item/item-list-item.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-item-list-item',
+  selector: 'bwm-item-list-item',
   templateUrl: './item-list-item.component.html',
   styleUrls: ['./item-list-item.component.css']
 })
